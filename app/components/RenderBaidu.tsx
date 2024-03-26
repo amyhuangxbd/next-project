46c4e6a4e1830bc7a6dae19f56e89fbcdce2455d
hebe huang <huanghx2018@qq.com>
2024-03-26 12:32:33 +0800

    add: 缓存 revalidate

--- app/components/RenderBaidu.tsx
+++ app/components/RenderBaidu.tsx
@@ -1,15 +1,14 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 
-function RenderBaidu() {
-    const data = React.use( fetch("https://api.vvhan.com/api/text/joke", { cache: 'no-cache' }).then(res => res.text()));
-    useEffect(() => {
-        console.log('rendering...')
-    }, [])
+function RenderBaidu(props: { txt: string }) {
+    const [count, setCount] = useState(0)
     return (
-        <div>
-            来着：{data}
+        <div onClick={() => {
+            setCount(count + 1)
+        }}>
+            来着：{props.txt} {count}
         </div>
     )
 }
--- app/discovery/page.tsx
+++ app/discovery/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import RenderBaidu from '../components/RenderBaidu'
 
 async function page() {
+    const data = await fetch('https://api.uomg.com/api/rand.qinghua', { next: { revalidate: 60 } }).then(res => res.json())
     return (
-        <RenderBaidu />
+        <RenderBaidu txt={data.content} />
     )
 }
 
